aa18eecb47a392f76a6b71bbff7e8e0d9862b9b9
geovannesdi@gmail.com <geovannesdi@gmail.com@535ce3a6-7f45-f405-f5f3-f03d800e99b5>
2015-03-13 13:23:41 +0000

    Marcação de branches Geovanne Alan

---
+++ branches/Geovanne&Alan/Geovanne&Alan/GerenciadorCliente.java
@@ -0,0 +1,20 @@
+public GerenciadorCliente{
+	
+	private String nomeCliente = null;
+	
+	public void inserir (String nomeCliente){
+
+	
+		this.nomeCliente = nomeCliente;
+	}
+	 public atualizar(){
+		 
+		 string nome;
+		 
+	 }
+		
+	
+	
+	
+	
+}
