4ee9e0af3df93d281fa272840d908dd82e6dc768
alanpassossi@gmail.com <alanpassossi@gmail.com@535ce3a6-7f45-f405-f5f3-f03d800e99b5>
2015-03-13 13:13:03 +0000

    Marcação da tag 1.0.Geovanne&Alan

---
+++ tags/1.0.GeovanneAlan/Dosea/GerenciadorCliente.java
@@ -0,0 +1,10 @@
+public class GerenciadorCliente {
+	public void inserir(Cliente cliente){
+		clientes.add(nomeCliente);
+	}
+
+	public boolean atualizar(Cliente cliente) {
+		return false;
+	}
+
+}
